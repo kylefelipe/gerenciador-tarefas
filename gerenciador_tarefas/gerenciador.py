fff0fc2e60fe9f94ecd4d0c99c871337a0b98aca
kylefelipe <kylefelipe@gmail.com>
2019-12-07 15:38:01 -0300

    Adicionando a criação de tarefas e os testes

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -1,18 +1,29 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, constr
+from uuid import UUID, uuid4
 
 app = FastAPI()
 
-class Tarefa(BaseModel):
+
+class TarefaEntrada(BaseModel):
     titulo: constr(min_length=3, max_length=50)
-    descricao: constr(max_length(140))
+    descricao: constr(max_length=140)
+
+
+class Tarefa(TarefaEntrada):
+    id: UUID
+
 
 TAREFAS = []
+
 
 @app.get('/tarefas')
 def listar():
     return TAREFAS
 
-@app.post('/tarefas')
-def criar(tarefa: Tarefa):
-    pass
+
+@app.post('/tarefas', response_model=Tarefa)
+def criar(tarefa: TarefaEntrada):
+    nova_tarefa = tarefa.dict()
+    nova_tarefa.update({"id": uuid4()})
+    return nova_tarefa
--- tests/test_gerenciador.py
+++ tests/test_gerenciador.py
@@ -3,20 +3,24 @@ from gerenciador_tarefas.gerenciador import app, TAREFAS
 
 # Testando o GET
 
+
 def test_quando_listar_tarefas_devo_ter_como_retorno_de_status200():
     cliente = TestClient(app)
     resposta = cliente.get("/tarefas")
     assert resposta.status_code == 200
+
 
 def test_quando_listar_tarefas_formato_deve_ser_json():
     cliente = TestClient(app)
     resposta =  cliente.get("/tarefas")
     assert resposta.headers["Content-Type"] == "application/json"
 
+
 def test_quando_listar_tarefas_retorno_deve_ser_uma_lista():
     cliente = TestClient(app)
     resposta = cliente.get("/tarefas")
     assert isinstance(resposta.json(), list)
+
 
 def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_id():
     TAREFAS.append({"id": 1})
@@ -25,6 +29,7 @@ def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_id():
     assert "id" in resposta.json().pop()
     TAREFAS.clear()
 
+
 def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_titulo():
     TAREFAS.append({"titulo": 'titulo 1'})
     cliente = TestClient(app)
@@ -32,12 +37,14 @@ def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_titulo():
     assert "titulo" in resposta.json().pop()
     TAREFAS.clear()
 
+
 def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_descricao():
     TAREFAS.append({"descricao": "descricao 1"})
     cliente = TestClient(app)
     resposta = cliente.get("/tarefas")
     assert "descricao" in resposta.json().pop()
     TAREFAS.clear()
+
 
 def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_um_estado():
     TAREFAS.append({"estado": "finalizado"})
@@ -52,11 +59,15 @@ def test_recurso_tarefas_deve_aceitar_o_verbo_post():
     cliente = TestClient(app)
     resposta = cliente.post("/tarefas")
     assert resposta.status_code != 405
+    TAREFAS.clear()
+
 
 def test_quando_uma_tarefa_e_submetida_deve_possuir_um_titulo():
     cliente = TestClient(app)
     resposta = cliente.post("/tarefas", json={})
     assert resposta.status_code == 422
+    TAREFAS.clear()
+
 
 def test_titulo_da_tarefa_deve_possuir_entre_3_e_50_caracteres():
     cliente = TestClient(app)
@@ -65,7 +76,28 @@ def test_titulo_da_tarefa_deve_possuir_entre_3_e_50_caracteres():
     resposta = cliente.post("/tarefas", json={"titulo": 51 * "*"})
     assert resposta.status_code == 422
 
+
 def test_descricao_da_tarefa_pode_conter_no_maximo_140_caracteres():
     cliente = TestClient(app)
     resposta = cliente.post("/tarefas", json={"titulo": "titulo", "descicao": "*" * 141})
     assert resposta.status_code == 422
+    TAREFAS.clear()
+
+
+def test_quando_criar_uma_tarefa_a_mesma_deve_ser_retornada():
+    cliente = TestClient(app)
+    tarefa = {"titulo": "titulo", "descricao": "descricao"}
+    resposta = cliente.post("/tarefas", json=tarefa)
+    print(resposta.json(), "\n", res)
+    assert resposta.json() == tarefa
+    TAREFAS.clear()
+
+
+def test_quando_criar_uma_tarefa_seu_id_deve_ser_unico():
+    cliente = TestClient(app)
+    tarefa1 = {"titulo": "titulo1", "descricao": "descricao1"}
+    tarefa2 = {"titulo": "titulo2", "descricao": "descricao2"}
+    resposta1 = cliente.post("/tarefas", json=tarefa1)
+    resposta2 = cliente.post("/tarefas", json=tarefa2)
+    assert resposta1.json()["id"] != resposta2.json()["id"]
+    TAREFAS.clear()
